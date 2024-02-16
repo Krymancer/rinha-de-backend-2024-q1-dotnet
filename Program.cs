4543ce3fdd59ef347e36db51d83f4b700d3ad59c
Júnior Nascimento <junior.nascm@gmail.com>
2024-02-16 01:18:19 -0300

    feat: hack to prevent insta 400 Bad Rquest on dotnet JsonDeserializer

--- Program.cs
+++ Program.cs
@@ -1,31 +1,18 @@
-using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Diagnostics;
 using Npgsql;
 
 string connectionString = "Host=db;Port=5432;Database=db;Username=user;Password=password;";
-
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.UseExceptionHandler(c => c.Run(async context =>
-{
-    var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
-    
-    if (exception is BadHttpRequestException badHttpRequestException)
-    {
-        context.Response.StatusCode = StatusCodes.Status422UnprocessableEntity;
-        await context.Response.CompleteAsync();
-    }
-}));
+var app = builder.Build();
 
 app.MapPost("/clientes/{id}/transacoes", async (int id, TransacaoPayload request) =>
 {
     if (id < 1 || id > 5) return Results.StatusCode(StatusCodes.Status404NotFound);
 
     if (
-        request is null || 
-        request.Tipo != 'd' && request.Tipo != 'c' || 
-        request.Valor <= 0 || 
+        request is null ||
+        request.Tipo != 'd' && request.Tipo != 'c' ||
+        request.Valor <= 0 || !(request.Valor % 1 == 0) ||
         request.Descricao is null || request.Descricao.Length < 1 || request.Descricao.Length > 10
         )
     {
@@ -43,25 +30,29 @@ app.MapPost("/clientes/{id}/transacoes", async (int id, TransacaoPayload request
 
         if (!read.Read()) return Results.StatusCode(StatusCodes.Status404NotFound);
 
-        Console.WriteLine(id);
-
         var saldo_cliente = read.GetInt32(0);
         var limite_cliente = read.GetInt32(1);
         read.Close();
 
-        if(request.Tipo == 'c') {
-            saldo_cliente += request.Valor;
-        }else if(saldo_cliente - request.Valor < limite_cliente * -1) {
+        if (request.Tipo == 'c')
+        {
+            saldo_cliente += (int)request.Valor;
+        }
+        else if (saldo_cliente - request.Valor < limite_cliente * -1)
+        {
             return Results.StatusCode(StatusCodes.Status422UnprocessableEntity);
-        }else {
-            saldo_cliente -= request.Valor;
+        }
+        else
+        {
+            saldo_cliente -= (int)request.Valor;
         }
 
-        using(var transaction = connection.BeginTransaction()) {
+        using (var transaction = connection.BeginTransaction())
+        {
             query = "INSERT INTO Transacao (valor, tipo, descricao, cliente_id) VALUES (@valor, @tipo, @descricao, @cliente_id)";
             using (command = new NpgsqlCommand(query, connection))
             {
-                command.Parameters.AddWithValue("valor", request.Valor);
+                command.Parameters.AddWithValue("valor", (int)request.Valor);
                 command.Parameters.AddWithValue("tipo", request.Tipo);
                 command.Parameters.AddWithValue("descricao", request.Descricao);
                 command.Parameters.AddWithValue("cliente_id", id);
@@ -140,5 +131,5 @@ app.MapGet("/clientes/{id}/extrato", async (int id) =>
     }
 });
 
-await app.RunAsync("http://0.0.0.0:99");
-public record TransacaoPayload([Required]int Valor, [Required]char Tipo, [Required]string Descricao);
+await app.RunAsync("http://0.0.0.0:3000");
+public record TransacaoPayload(float Valor, char Tipo, string Descricao);
